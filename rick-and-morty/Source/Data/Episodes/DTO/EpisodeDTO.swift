87c1b7a4ae281d8417ee721ecf8d0f3bf6d045ad
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-09-19 11:34:26 +0200

    * Episodes Module: Changes MR

--- rick-and-morty/Source/Data/Episodes/DTO/EpisodeDTO.swift
+++ rick-and-morty/Source/Data/Episodes/DTO/EpisodeDTO.swift
@@ -10,9 +10,19 @@ import Foundation
 struct EpisodeDTO: Decodable {
     let id: Int
     let name: String
-    let air_date: String
+    let airDate: String
     let episode: String
     let characters: [String]
     let url: String
     let created: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case airDate = "air_date"
+        case episode
+        case characters
+        case url
+        case created
+    }
 }
