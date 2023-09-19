f583e8ac96c385ccd8dcd74555bff500e056d45b
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-09-19 15:06:43 +0200

    * Episode Detail Module: Creating folders and files

---
+++ rick-and-morty/Source/Module/EpisodeDetail/Presenter/EpisodeDetailPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  EpisodeDetailPresenter.swift
+//  rick-and-morty
+//
+//  Created by Carlos Morgado on 19/9/23.
+//
+
+import Foundation
---
+++ rick-and-morty/Source/Module/EpisodeDetail/Router/EpisodeDetailRouter.swift
@@ -0,0 +1,8 @@
+//
+//  EpisodeDetailRouter.swift
+//  rick-and-morty
+//
+//  Created by Carlos Morgado on 19/9/23.
+//
+
+import Foundation
